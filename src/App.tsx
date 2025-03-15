76c2c08d49569fb2063d1584cdcb38f806b27869
Ricciflows <1206411001@qq.com>
2025-03-16 01:39:11 +0800

    inital_commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import "@ant-design/v5-patch-for-react-19";
+import { Button, ConfigProvider, Image, Typography } from "antd";
+import { ofetch } from "ofetch";
+
+function App() {
+  const [csrfToken, setCsrfToken] = useState("");
+
+  useEffect(() => {
+    ofetch("/api/get_csrf_token/").then((res) => {
+      setCsrfToken(res.csrftoken);
+    });
+  }, []);
+
+  const { Title } = Typography;
+
+  async function Download() {
+    ofetch(`/apim/MobileAPPVersions/app/download_lastest_app/`, {
+      method: "PATCH",
+      headers: {
+        "X-CSRFToken": csrfToken,
+      },
+      body: {
+        platform: "android",
+      },
+    });
+
+    const a = document.createElement("a"); //创建一个<a></a>标签
+    a.href = "/弦圈-V0.0.2-正式版.apk";
+    //给a标签的href属性值加上地址，注意，这里是绝对路径，不用加 点.
+    a.download = "弦圈_V0.0.2_正式版.apk";
+    //设置下载文件文件名，这里加上.xlsx指定文件类型，pdf文件就指定.fpd即可
+    a.style.display = "none"; // 障眼法藏起来a标签
+    document.body.appendChild(a);
+    // 将a标签追加到文档对象中
+    a.click(); //模拟点击了a标签，会触发a标签的href的读取，浏览器就会自动下载了
+    a.remove();
+    // 一次性的，用完就删除a标签
+  }
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <a href="https://www.xianquan.net">
+        <Image
+          src="/manitori.png"
+          width={300}
+          height={100}
+          style={{ objectFit: "cover" }}
+          preview={false}
+        ></Image>
+      </a>
+      <Title level={2}>V0.0.2</Title>
+
+      <Title>🚀下载弦圈APP🎉</Title>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#ff6b81",
+          },
+        }}
+      >
+        <Button type="primary" size="large" onClick={Download}>
+          立即下载
+        </Button>
+      </ConfigProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    cors: true, // 默认启用并允许任何源
+    //反向代理配置，注意rewrite写法
+    proxy: {
+      "/apim": {
+        target: "https://api.manitori.xyz", //代理接口
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/apim/, "api-mobile"),
+      },
+      "/api": {
+        target: "https://api.manitori.xyz", //代理接口
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "api"),
+      },
+    },
+  },
+});
