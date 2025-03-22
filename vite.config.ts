d4a6cca1e9985a5158495ffb329ad49aa7cd3891
Ricciflows <149518652+Ricciflows@users.noreply.github.com>
2025-03-23 01:42:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/xianquan-app-download/",
-  server: {
-    cors: true, // 默认启用并允许任何源
-    //反向代理配置，注意rewrite写法
-    proxy: {
-      "/apim": {
-        target: "https://api.manitori.xyz", //代理接口
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apim/, "api-mobile"),
-      },
-      "/api": {
-        target: "https://api.manitori.xyz", //代理接口
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "api"),
-      },
-    },
-  },
+  base: "/xianquan-app-download/"
 });
